4350fdda1397cd80c4254d29e3398a24e6a3b360
poohwaa <poohwaa@gmail.com>
2024-02-29 19:17:46 +0900

    client fetch

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,25 @@
-export const metadata = {
-    title: 'Home',
-}
+"use client";
+
+import { useEffect, useState } from "react"
 
 export default function Hello() {
+    const [movies, setMovies] = useState([]);
+    const [isLoading, setLoading] = useState(true);
+
+    const getMovies = async () => {
+        const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
+        const json = await response.json();
+        setMovies(json);
+        setLoading(false);
+        // console.log(json);
+    }
+
+    useEffect(() => {
+        getMovies();
+    }, []);
     return (
         <div>
-            <h1>Hello Next</h1>
+            <h1>{isLoading ? "loading..." : JSON.stringify(movies)}</h1>
         </div>
     )
 }
