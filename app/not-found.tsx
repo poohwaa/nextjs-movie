9c4742fbedfcfa3e2df74a409670db809aea59ec
poohwaa <poohwaa@gmail.com>
2024-02-29 14:29:00 +0900

    first commit

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/Navigation";
+
+export default function NotFound() {
+    return (
+        <div>
+            <Navigation />
+            <h1>NOT FOUNT!</h1>
+        </div>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navigation from "../components/Navigation"
+
+export default function Hello() {
+    return (
+        <div>
+            <Navigation />
+            <h1>Hello Next</h1>
+        </div>
+    )
+}
