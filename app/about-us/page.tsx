193a6fe8dd5673fc434a2db6b406f70d6fd9c6e5
poohwaa <poohwaa@gmail.com>
2024-03-01 20:04:51 +0900

    update about us

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -7,7 +7,10 @@ export default function AboutUs() {
     // setTimeout(() => console.log("Aboutus"), 10000);
     return (
         <div>
-            <h1>About us~</h1>
+            <h1>💖우리 가족💖</h1>
+            <p>장녀: 배 메이 초등학교 6학년 짱 예쁨 (❁´◡`❁)</p>
+            <p>차녀: 배 레이 초등학교 4학년 짱 귀여움 ╰(*°▽°*)╯</p>
+            <p>장남: 배 카이리 초등학교 1학년 짱 멋있음 (●'◡'●)</p>
         </div>
     )
 }
